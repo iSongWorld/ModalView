6fa538b70331e8c0b4b47b8d4a140ba428eb96d0
陈荣雄 <chenrx@foxmail.com>
2015-07-03 10:56:47 +0800

    Init Project

---
+++ ModalView/ModalView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModalView
+//
+//  Created by 陈荣雄 on 15/7/3.
+//  Copyright (c) 2015年 陈荣雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModalView/ModalView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ModalView
+//
+//  Created by 陈荣雄 on 15/7/3.
+//  Copyright (c) 2015年 陈荣雄. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
